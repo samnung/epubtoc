5ba8f5ac1c682d7151f78fa73bef58a55cdc7ca0
Roman Kříž <samnung@gmail.com>
2013-03-09 21:44:17 +0100

    added converting to xhtml

--- ncx2xhtml.py
+++ ncx2xhtml.py
@@ -10,8 +10,19 @@ def stringMultiply(text, count):
 	return retText
 
 
-class Toc(object):
+def removePrefix(name):
+	if name[0] == '{':
+		uri, tag = name[1:].split('}')
+		return tag
+	else:
+		return name
 
+def addNcxPrefix(name):
+	return '{http://www.daisy.org/z3986/2005/ncx/}' + name
+
+
+class Toc(object):
+	''' Class representing TOC '''
 	def __init__(self):
 		super(Toc, self).__init__()
 		self.levels = list()
@@ -27,13 +38,25 @@ class Toc(object):
 
 			self.levels.append(level)
 
+	def toXHTML(self):
+
+		ol = ElementTree.Element('ol')
+
+		for level in self.levels:
+			level.toXHTML(ol)
+
+		return ol
+
+
 	def printTOC(self):
 		for level in self.levels:
 			level.printTree()
 
 
-class TocLevel(object):
 
+
+class TocLevel(object):
+	''' Class representing one TOC level (h1, h2, h3, ...) '''
 	def __init__(self):
 		super(TocLevel, self).__init__()
 		self.text = ''
@@ -57,6 +80,20 @@ class TocLevel(object):
 			self.addSubLevel(new)
 
 
+	def toXHTML(self, parent):
+
+		root = ElementTree.SubElement(parent, 'li')
+		a = ElementTree.SubElement(root, 'a', {'href' : self.href})
+		a.text = self.text
+
+		if len(self.subLevels) > 0:
+
+			ol = ElementTree.SubElement(root, 'ol')
+
+			for level in self.subLevels:
+				level.toXHTML(ol)
+
+
 	def addSubLevel(self, level):
 			self.subLevels.append(level)
 
@@ -68,15 +105,6 @@ class TocLevel(object):
 
 
 
-def removePrefix(name):
-	if name[0] == '{':
-		uri, tag = name[1:].split('}')
-		return tag
-	else:
-		return name
-
-def addNcxPrefix(name):
-	return '{http://www.daisy.org/z3986/2005/ncx/}' + name
 
 
 
@@ -87,7 +115,11 @@ document = ElementTree.parse('toc.ncx')
 
 toc = Toc()
 toc.parseFromRootNCX(document.getroot())
-toc.printTOC()
+
+
+xhtml = toc.toXHTML()
+
+print(ElementTree.tostring(xhtml, encoding='unicode'))
 
 
 
