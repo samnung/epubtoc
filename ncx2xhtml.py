02683af51477109a615b566a4a93606bc89e90bf
Roman Kříž <samnung@gmail.com>
2013-03-10 22:52:28 +0100

    printing html header

--- ncx2xhtml.py
+++ ncx2xhtml.py
@@ -3,13 +3,6 @@ from xml.etree import ElementTree
 import sys
 
 
-def stringMultiply(text, count):
-	retText = ''
-	for x in range(0, count):
-		retText += text
-
-	return retText
-
 
 def removePrefix(name):
 	if name[0] == '{':
@@ -101,7 +94,7 @@ class TocLevel(object):
 
 
 	def printTree(self, indent = 0):
-		print(stringMultiply('   ', indent), self.text, ':  ', self.href)
+		print('   '*indent, self.text, ':  ', self.href)
 		for sub in self.subLevels:
 			sub.printTree(indent + 1)
 
@@ -113,7 +106,7 @@ if __name__ == '__main__':
 
 	if len(sys.argv) is not 2:
 		print('usage ncx2xhtml.py <file.ncx>', file=sys.stderr)
-		sys.exit(0)
+		sys.exit(0 if len(sys.argv) is 1 else 1)
 
 	else:
 		try:
@@ -124,12 +117,31 @@ if __name__ == '__main__':
 
 
 
-
 	toc = Toc(document.getroot())
+
+
+	header = '''<?xml version="1.0" encoding="UTF-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+<head>
+	<title></title>
+</head>
+<body>
+
+<nav epub:type="toc">
+'''
+	footer = '''</nav>
+
+</body>
+</html>
+'''
 
 	xhtml = toc.toXHTML()
 
+
+
+	print(header)
 	print(ElementTree.tostring(xhtml, encoding='unicode'))
+	print(footer)
 
 
 
